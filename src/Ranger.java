3211e7033274ecd463ca2e9760dc8fec67dcd1b4
Avery Bainbridge <cattata1056@gmail.com>
2016-12-06 20:18:26 -0700

    Fixed issues related to health and gun feedback

--- src/Ranger.java
+++ src/Ranger.java
@@ -3,31 +3,35 @@ public class Ranger extends Character {
     private double hitChance;
     private int numBullets = 15;
     private boolean isGunClean = true;
-    Ranger(String name, double hitChance){
+
+    Ranger(String name, double hitChance) {
         super(name);
-        this.hitChance=hitChance; //ratio of hits to misses
+        this.hitChance = hitChance; //ratio of hits to misses
     }
+
     @Override
     public void attack(Character other) {
-        if(isGunClean) {
+        if (isGunClean) {
             if (numBullets != 0) {
-                other.health = (int) (Math.random() * 5 * (hitChance * 3));
+                other.health -= (int) (Math.random() * 5 * (hitChance * 3));
                 numBullets--;
-                if(Math.random() > .99){
+                System.out.println("bang!");
+                if (Math.random() > .99) {
                     isGunClean = false;
                 }
             } else {
                 System.out.println("*click*"); //clip is empty, reload
             }
-        }else{
+        } else {
             this.health -= 10;
             System.out.println("boom!");
             //gun explodes if barrel is obstructed
         }
     }
+
     @Override
     public void describeSelf() {
-        System.out.println("Hello, my name is "+ getName() +". I'm a Ranger.");
+        System.out.println("Hello, my name is " + getName() + ". I'm a Ranger.");
     }
 
     @Override
@@ -39,7 +43,7 @@ public class Ranger extends Character {
         numBullets = 15;
     }
 
-    public void clean(){
+    public void clean() {
         isGunClean = true;
     }
 }
