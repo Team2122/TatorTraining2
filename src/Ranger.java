36961300fb3c6f0af6da3388c336be2f01cabae4
Avery Bainbridge <cattata1056@gmail.com>
2016-12-06 19:55:09 -0700

    Updated Ranger class

--- src/Ranger.java
+++ src/Ranger.java
@@ -8,16 +8,24 @@ public class Ranger extends Character {
     }
     @Override
     public void attack(Character other) {
-        other.health=(int)(Math.random() * 5 * (hitChance*3));
-        numBullets--;
+        if(numBullets!=0) {
+            other.health = (int) (Math.random() * 5 * (hitChance * 3));
+            numBullets--;
+        }else{
+            System.out.println("*click*");
+        }
     }
     @Override
     public void describeSelf() {
-
+        System.out.println("Hello, my name is "+getName()+". I'm a Ranger.");
     }
 
     @Override
     public String getPrimaryWeapon() {
-        return null;
+        return "Rifle";
+    }
+
+    public void reload() {
+        numBullets=15;
     }
 }
