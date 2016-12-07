3f36ba1fe40515f26e47b76eaba63a43aad78e3c
Avery Bainbridge <cattata1056@gmail.com>
2016-12-06 20:13:20 -0700

    Added gun cleaning, made code more readable

--- src/Ranger.java
+++ src/Ranger.java
@@ -1,23 +1,33 @@
 
 public class Ranger extends Character {
     private double hitChance;
-    private int numBullets=15;
+    private int numBullets = 15;
+    private boolean isGunClean = true;
     Ranger(String name, double hitChance){
         super(name);
         this.hitChance=hitChance; //ratio of hits to misses
     }
     @Override
     public void attack(Character other) {
-        if(numBullets!=0) {
-            other.health = (int) (Math.random() * 5 * (hitChance * 3));
-            numBullets--;
+        if(isGunClean) {
+            if (numBullets != 0) {
+                other.health = (int) (Math.random() * 5 * (hitChance * 3));
+                numBullets--;
+                if(Math.random() > .99){
+                    isGunClean = false;
+                }
+            } else {
+                System.out.println("*click*"); //clip is empty, reload
+            }
         }else{
-            System.out.println("*click*"); //clip is empty, reload
+            this.health -= 10;
+            System.out.println("boom!");
+            //gun explodes if barrel is obstructed
         }
     }
     @Override
     public void describeSelf() {
-        System.out.println("Hello, my name is "+getName()+". I'm a Ranger.");
+        System.out.println("Hello, my name is "+ getName() +". I'm a Ranger.");
     }
 
     @Override
@@ -26,6 +36,10 @@ public class Ranger extends Character {
     }
 
     public void reload() {
-        numBullets=15;
+        numBullets = 15;
+    }
+
+    public void clean(){
+        isGunClean = true;
     }
 }
