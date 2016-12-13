753f36fa0f6a98bc9f3aed4045648dde1acb6466
Avery Bainbridge <cattata1056@gmail.com>
2016-12-12 20:02:37 -0700

    Fixed whitespace issues

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,32 @@
 import java.util.ArrayList;
 
-/**
- * Created by avery on 12/6/16.
- */
 public class Main {
-
     public static void main(String[] args) {
         ArrayList<Character> characters = new ArrayList();
-        characters.add(new Ranger("Ranger 1", 0.65));
-        characters.add(new Ranger("Ranger 2", .75));
-        characters.add(new Ranger("Ranger 3", .50));
-
-        for(Character character : characters){
+        characters.add(new Ranger("Alex", .01));
+        characters.add(new Ranger("Jimmy", .75));
+        characters.add(new Ranger("Jake", .50));
+        for (Character character : characters) {
             character.describeSelf();
         }
         for (int i = 0; characters.size() != 1; i++) {
             if (i >= characters.size()) {
                 i = 0;
             }
-            if (!characters.get(i).isAlive()) {
-                System.out.println("Removed the body of " + characters.get(i).getName());
+            Character curr = characters.get(i);
+            if (!curr.isAlive()) {
+                System.out.println("Removed the body of " + curr.getName());
                 characters.remove(i);
                 i--;
                 continue;
             }
-            int targetIndex = characters.get(i).pickTarget(characters);
+            int targetIndex = curr.pickTarget(characters);
             if (targetIndex != i) {
-                System.out.println(characters.get(i).getName() + " attacked " +
+                System.out.println(curr.getName() + " attacked " +
                         characters.get(targetIndex).getName() +
-                        " with their " + characters.get(i).getEquippedWeapon().getName());
+                        " with their " + curr.getEquippedWeapon().getName());
             }
-            characters.get(i).getEquippedWeapon().useOn(characters.get(targetIndex));
-
+            curr.getEquippedWeapon().useOn(characters.get(targetIndex));
         }
         System.out.println(characters.get(0).getName() + " won!");
     }
